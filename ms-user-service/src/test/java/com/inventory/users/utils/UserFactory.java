269ea585a9a123fb1ff038091681a9161ba269d8
Deborah Gonzalez <v-debogonzalez@expediagroup.com>
2025-09-16 16:40:23 -0300

    [ADD] add cypher method in test for ddbb users user-service

--- ms-user-service/src/test/java/com/inventory/users/utils/UserFactory.java
+++ ms-user-service/src/test/java/com/inventory/users/utils/UserFactory.java
@@ -2,6 +2,7 @@ package com.inventory.users.utils;
 
 import com.inventory.users.domain.entity.User;
 import com.inventory.users.domain.entity.UserRole;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserFactory {
 
@@ -11,7 +12,6 @@ public class UserFactory {
         user.setUsername("testUser");
         user.setPassword("Abc123");
         user.setEmail("testUser@gmail.com");
-        user.setPassword("Abc123*");
         user.setRole(UserRole.USER);
         return user;
     }
@@ -22,8 +22,20 @@ public class UserFactory {
         user.setUsername("testAdmin");
         user.setPassword("Abc123");
         user.setEmail("testAdmin@gmail.com");
-        user.setPassword("Abc123!");
         user.setRole(UserRole.ADMIN);
         return user;
+    }
+
+    public static String encryptPassword(String plainPassword) {
+        String encryptedPassword = new BCryptPasswordEncoder().encode(plainPassword);
+        System.out.println("Encrypted password: " + encryptedPassword);
+        return encryptedPassword;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Test user debi:");
+        encryptPassword("Test123*");
+        System.out.println("Test admin santi:");
+        encryptPassword("Test123!");
     }
 }
