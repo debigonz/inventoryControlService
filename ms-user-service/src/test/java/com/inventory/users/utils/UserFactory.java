6d97958b3d0f944e53b58f1a06a121044e98c081
Deborah Gonzalez <v-debogonzalez@expediagroup.com>
2025-09-16 16:55:20 -0300

    [MOD] passwords users

--- ms-user-service/src/test/java/com/inventory/users/utils/UserFactory.java
+++ ms-user-service/src/test/java/com/inventory/users/utils/UserFactory.java
@@ -2,7 +2,6 @@ package com.inventory.users.utils;
 
 import com.inventory.users.domain.entity.User;
 import com.inventory.users.domain.entity.UserRole;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class UserFactory {
 
@@ -26,16 +25,15 @@ public class UserFactory {
         return user;
     }
 
-    public static String encryptPassword(String plainPassword) {
-        String encryptedPassword = new BCryptPasswordEncoder().encode(plainPassword);
-        System.out.println("Encrypted password: " + encryptedPassword);
-        return encryptedPassword;
-    }
-
     public static void main(String[] args) {
+        java.util.function.Function<String, String> encryptPassword = plainPassword -> {
+            String encryptedPassword = new org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder().encode(plainPassword);
+            System.out.println("Encrypted password: " + encryptedPassword);
+            return encryptedPassword;
+        };
         System.out.println("Test user debi:");
-        encryptPassword("Test123*");
+        encryptPassword.apply("Test123*");
         System.out.println("Test admin santi:");
-        encryptPassword("Test123!");
+        encryptPassword.apply("Test123!");
     }
 }
