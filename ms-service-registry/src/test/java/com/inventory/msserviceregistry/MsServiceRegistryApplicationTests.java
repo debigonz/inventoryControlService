aa1550c58468f564167e497c696e6c65acf98a49
Deborah Gonzalez <v-debogonzalez@expediagroup.com>
2025-10-20 20:27:16 -0300

    [ADD] service registry

---
+++ ms-service-registry/src/test/java/com/inventory/msserviceregistry/MsServiceRegistryApplicationTests.java
@@ -0,0 +1,14 @@
+package com.inventory.msserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsServiceRegistryApplicationTests {
+
+    @Test
+    void contextLoads() {
+        //
+    }
+
+}
