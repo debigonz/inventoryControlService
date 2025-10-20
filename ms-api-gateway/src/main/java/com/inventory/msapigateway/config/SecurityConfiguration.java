23dfa044fc3fc4dfb11610137f27456c68adeae2
Deborah Gonzalez <v-debogonzalez@expediagroup.com>
2025-10-20 14:33:41 -0300

    [MOD] api gateway as a resource server

--- ms-api-gateway/src/main/java/com/inventory/msapigateway/config/SecurityConfiguration.java
+++ ms-api-gateway/src/main/java/com/inventory/msapigateway/config/SecurityConfiguration.java
@@ -1,39 +1,22 @@
 package com.inventory.msapigateway.config;
 
-
-import lombok.AllArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 
-@AllArgsConstructor
-@RequiredArgsConstructor
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfiguration {
 
-    private ServerAuthenticationConverter authenticationConverter;
-    private ReactiveAuthenticationManager manager;
-
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http, ReactiveClientRegistrationRepository client) {
-        http.oauth2Login(oauth2Login ->
-                oauth2Login
-                        .authenticationConverter(authenticationConverter)
-                        .authenticationManager(manager));
-        http.logout(logoutSpec -> logoutSpec.logoutSuccessHandler(
-                new OidcClientInitiatedServerLogoutSuccessHandler(client)
-        ));
-        http.authorizeExchange(authorize ->
-                authorize
-                        .anyExchange()
-                        .authenticated());
-        http.cors(ServerHttpSecurity.CorsSpec::disable);
+    SecurityWebFilterChain securityFilterChain (ServerHttpSecurity http) {
+        http.authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.anyExchange().authenticated())
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+
         return http.build();
     }
 }
